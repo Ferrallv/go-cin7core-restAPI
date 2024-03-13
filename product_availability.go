0d73dbcab66e4765b3f472236570bdcb14d37f90
Nin <vanin.ferrall@gmail.com>
2024-03-13 15:09:10 -0400

    adding a ListAll function for pagination. More testing to be done and better functionality

--- product_availability.go
+++ product_availability.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 const product_availability_url_slug = "ref/productavailability"
 
 type ProductAvailabilityService interface {
 	List(options map[string]string) ([]ProductAvailability, error)
+	ListAll(options map[string]string) ([]ProductAvailability, error)
 }
 
 type ProductAvailabilityOp struct {
@@ -29,6 +31,61 @@ func (s *ProductAvailabilityOp) List(options map[string]string) ([]ProductAvaila
 	}
 	product_availability = cin7_resp.ProductAvailabilityList
 	return product_availability, nil
+}
+
+func (s *ProductAvailabilityOp) ListAll(options map[string]string) ([]ProductAvailability, error) {
+	var product_availability []ProductAvailability
+	endpoint := s.client.BuildCin7Endpoint(product_availability_url_slug, options)
+	req, err := s.client.BuildCin7Request("GET", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+	cin7_resp, err := s.ReturnPaginatedCin7Data(req)
+	if err != nil {
+		return nil, err
+	}
+	product_availability = cin7_resp.ProductAvailabilityList // Block to loop through requests and get paginated data
+
+	// Check if limit is explicit, set to default for cin7 otherwise
+	if _, ok := options["Limit"]; !ok {
+		options["Limit"] = "100"
+	}
+	// limit of loops here in case  something bad happens
+	count := 0
+	for {
+		count += 1
+		// calculate how many records are left
+		set_limit, err := strconv.ParseInt(options["Limit"], 0, 64)
+		if err != nil {
+			return nil, err
+		}
+		remainder := cin7_resp.Total - (int(set_limit) * cin7_resp.Page)
+		fmt.Println(remainder)
+		if remainder < 1 {
+			// got all records
+			return product_availability, nil
+		} else {
+			// set options to next page for request
+			options["Page"] = strconv.Itoa(cin7_resp.Page + 1)
+		}
+
+		// do the request loop again
+		endpoint := s.client.BuildCin7Endpoint(product_availability_url_slug, options)
+		req, err := s.client.BuildCin7Request("GET", endpoint, nil)
+		if err != nil {
+			return nil, err
+		}
+		cin7_resp, err = s.ReturnPaginatedCin7Data(req)
+		if err != nil {
+			return nil, err
+		}
+		// append our list of paginated values
+		product_availability = append(product_availability, cin7_resp.ProductAvailabilityList...)
+
+		if count > 4 {
+			return nil, fmt.Errorf("Shit got round too many times during testing, stop battering API")
+		}
+	}
 }
 
 func (s *ProductAvailabilityOp) ReturnPaginatedCin7Data(req *http.Request) (*ProductAvailablityPaginatedResponse, error) {
